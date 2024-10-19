bd381afaa7dc6f38db730869fe4fbf66a632d295
Neddam <bradymadden86@gmail.com>
2024-10-19 12:39:34 -0500

    updating function and notes

--- Cantor.py
+++ Cantor.py
@@ -1,15 +1,18 @@
 from tkinter import *
 
 
-def cantor_set(x,y,l):
-    if l > 1: #keep doing this while the line is > 1 pixel lon
-        win.create_line(x,y,x+l,y) #draw horizontal lin
-        y = y + 50 #move 50 pixels down for next generation
-        cantor_set(x,y,l/3) #left hand offspring
-        cantor_set(x+2/3*l,y,l/3) #right hand offspring
+def cantor_set(x_cord,y_cord,leng):
+    if leng > 1: #keep doing this while the line is > 1 pixel lon
+        win.create_line(x_cord,y_cord,x_cord+leng,y_cord) #draw horizontal lin
+        y_cord = y_cord + 50 #move 50 pixels down for next generation
+        cantor_set(x_cord,y_cord,leng/3) #left hand offspring
+        cantor_set(x_cord+2/3*leng,y_cord,leng/3) #right hand offspring
 
 
-w,h = 1500,500
-win = Canvas(Tk(),width=w,height=h)
+width,height = 1500,500
+root = Tk()
+win = Canvas(root,width=width,height=height)
 win.pack()
-cantor_set(10,10,w-20)
+cantor_set(10,10,width-20)
+
+root.mainloop()
